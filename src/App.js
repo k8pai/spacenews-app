9096b91a44049ae2525c47569d24225c28e84a22
k8pai <thek8pai@gmail.com>
2023-02-08 11:25:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [news, setNews] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.spaceflightnewsapi.net/v3/articles')
+			.then(response => response.json())
+			.then(data => {
+				setNews(data);
+			})
+	})
+
+	return (
+		<div className="App">
+			<div>
+				<h1>space news</h1>
+			</div>
+			<div className='newsArticles'>
+				{news && news?.map(data => {
+					return (
+						<div className='news-card' key={data.id}> 
+							<h3>{data.title}</h3>
+							<p>{data.summary}</p>
+							<p>{data.publishedAt}</p>
+							<a href={data.url}>visit page</a>
+						</div>
+					)
+				})}
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
